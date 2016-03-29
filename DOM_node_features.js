2d963d38c1503ad09092431ed470f97fdf456fab
delta <Roma>
2016-03-29 15:25:09 +0300

    Changed behavior of all function which used getNodeLeafs and getNodeLeafsFromIter functions. Now they are returning real leafs nodes and not "almost leafs".
    Function getSelectedForest now implemented via new function
    leafsToForest. Also found and fixed bug in getSelectedForest - now
    result dont depend on direction of selection (order of anchorNode and
    focusNode).
    
    Written but not tested functions levenshteinDistanceForSequence and
    makeGeneratorOfPossibleMatches.

--- DOM_node_features.js
+++ DOM_node_features.js
@@ -1,3 +1,40 @@
+function realLeafToAlmostLeaf(node) {
+    // returns ancestors node such that it and all its descendants have only 1 child
+    function is_almost_leaf(node) {
+        if (! node.hasChildNodes() || node.children.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    while (node != win.document.documentElement && is_almost_leaf(node.parentNode)) {
+        node = node.parentNode;
+    }
+
+    return node;
+}
+
+function isLeafBeforeAnother(l1, l2) {
+    // work properly only if l1 and l2 are leafs
+    var tmp = l1.compareDocumentPosition(l2);
+    if (tmp === 4) {
+        return true;
+    } else if (tmp === 2) {
+        return false;
+    } else {
+        throw "one leafs is not before or after one another. That's really strange (if they are really leafs)";
+    }
+}
+
+function isLeafNodeInRange(firstLeafInRange, lastLeafInRange, node) {
+    if (firstLeafInRange === node || lastLeafInRange === node) {
+        return true;
+    } 
+
+    return isLeafBeforeAnother(firstLeafInRange, node) && isLeafBeforeAnother(node, lastLeafInRange);
+}
+
 function ancestors_n(doc, node, ancestors_number) {
     var result = [];
     while (node != doc.documentElement && ancestors_number > 0) {
@@ -19,14 +56,6 @@ function ancestors_all(doc, node) {
     return result;
 }
 
-function is_leaf(node) {
-    if (!node.hasChildNodes() || node.children.length === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 function getNodeLeafsFromIter(iterator, make_iterator = makeForwardIterator, maxLeafs = Infinity) {
     var result = [];
     var cur_res = iterator.next();
@@ -38,8 +67,8 @@ function getNodeLeafsFromIter(iterator, make_iterator = makeForwardIterator, max
     return result;
 }
 
-function getNodeLeafs(node, make_iterator, maxLeafs = Infinity) {
-    if (is_leaf(node)) {
+function getNodeLeafs(node, make_iterator = makeForwardIterator, maxLeafs = Infinity) {
+    if (node.childNodes.length == 0) {
         return [node];
     }
 
@@ -107,75 +136,152 @@ function makeBackwardIterator(array, zero_index = -1) {
     }    
 }
 
-
 function getSelectedForest(win) {
     var selection = win.getSelection();
-    var first_selection_node = selection.anchorNode;
-    if (first_selection_node === null) {
+    if (selection.anchorNode === null) {
         return [];
     }
-    
-    var result = [];
-    while (selection.containsNode(first_selection_node, true)) {
-        var cur_node = first_selection_node;
+ 
 
-        const treeRoot = win.document.documentElement;
+    var first = selection.anchorNode;
+    var last = selection.focusNode;
+
+    // first and last node of selection is always leafs
+    if (isLeafBeforeAnother(last, first)) {
+        var tmp;
+        tmp = first;
+        first = last;
+        last = tmp;
+    }
+
+    const wholeHTML = getNodeLeafs(win.document.documentElement);
+    const selectedLeafs = wholeHTML.filter(isLeafNodeInRange.bind(null, first, last));
+    return leafsToForest(win.document, selectedLeafs);
+}
+
+function leafsToForest(doc, leafsList) {
+    if (leafsList === []) {
+        return [];
+    }
+
+    var isOneOfGivenLeafs = isLeafNodeInRange.bind(null, leafsList[0], leafsList[leafsList.length - 1]);
+
+    var leftestUnprocessedNode = leafsList[0];
+    var result = [];
+    while (isOneOfGivenLeafs(leftestUnprocessedNode)) {
+        var cur_node = leftestUnprocessedNode;
+
+        const treeRoot = doc.documentElement;
         var rightestChild = function (node) {
             // assuming that node is legitimate node
-            // therefore there is at least 1 child
-            return getNodeLeafs(node, makeBackwardIterator)[0];
+            // therefore there is at least 1 child (or itself is leaf) 
+            return getNodeLeafs(node, makeBackwardIterator, 1)[0];
         }
 
-        while (cur_node != treeRoot && selection.containsNode(rightestChild(cur_node.parentNode), true)) {
+        var leftestChild = function (node) {
+            // assuming that node is legitimate node
+            // therefore there is at least 1 child (or itself is leaf)
+            return getNodeLeafs(node, makeForwardIterator, 1)[0];
+        }
+
+        while (cur_node != treeRoot && 
+               isOneOfGivenLeafs(rightestChild(cur_node.parentNode)) &&
+               isOneOfGivenLeafs(leftestChild(cur_node.parentNode))) {
             cur_node = cur_node.parentNode;
         }
 
         result.push(cur_node);
 
-        var nextNodeLst = next_n(win.document, cur_node, 1);
+        var nextNodeLst = next_n(doc, cur_node, 1);
         if (nextNodeLst.length === 0) {
             break;
         }
 
-        first_selection_node = nextNodeLst[0];
+        leftestUnprocessedNode = nextNodeLst[0];
     }
 
     return result;
 }
 
+// untested
+function levenshteinDistanceOfSequence(pattern, sequence) {
+    var result = []
+    var prevDistanceColumn = new Array(pattern.length + 1);
+    var curDistanceColumn = new Array(pattern.length + 1);
 
-/*
- * following functions may come in handy later
- * but for now they are just useless untested piece of code
- *
-function hashDistance(hash1, hash2) {
-    var result = 0;
-    for (var className in hash1) {
-        if (!hash1.hasOwnProperty(className)) {
-            continue;
-        }
-
-        if (hash2.hasOwnProperty(className)) {
-            result = result + abs(hash1.className - hash2.className);
-        } else {
-            result = result + hash1.className
-        }
+    for (var i = 0; i < prevDistanceColumn.length; i++) {
+        prevDistanceColumn[i] = i;
     }
-}
 
-function leafsListToLeafsSetHash(list) {
-   var hash = {};
-    for (var i = 0; i < list.length; i++) {
-        if (hash.hasOwnProperty(list[i].nodeName)) {
-            hash[list.nodeName]++;
-        } else {
-            hash[list.nodeName] = 1;
+    for (var i = 0; i < sequence.length; i++) {
+        curDistanceColumn[0] = 0;
+        for (var j = 1; j < curDistanceColumn.length; j++) {
+            var costOfChange = prevDistanceColumn[j];
+            if (pattern[j] != sequence[i]) {
+                costOfChange++;
+            }
+
+            curDistanceColumn[j] = min(costOfChange, min(prevDistance[j] + 1, curDistanceColumn[j - 1] + 1));
         }
     }
 
-    return hash;
+    return result;
 }
-*/
+
+// untested
+function* makeGeneratorOfPossibleMatches(doc, nodesList) {
+    function nodesToNodeTypes(array) {
+        return array.map(function (node) {
+            return node.nodeName;
+        });
+    }
+
+    const pattern = nodesToNodeTypes(nodesList);
+
+    const wholeHTML = getNodeLeafs(doc.documentElement);
+    var numberedSeq = levenshteinDistanceOfSequence(pattern, nodesToNodeTypes(wholeHTML)).map(function(item, index) {
+        return {
+            value: item, 
+            index: index
+        };
+    });
+    
+    numberedSeq.sort(function(a, b) {
+        if (a.value < b.value) {
+            return -1;
+        } else if (a.value == b.value) {
+            return 0;
+        } else {
+            return 1;
+        }
+    });
+
+    var takenIndexes = [];
+
+    function isIndexTaken(index) {
+        for (var i = 0; i < takenIndexes.length; i++) {
+            if (index - takenIndexes[i] > 0 && index - takenIndexes[i] < pattern.length) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    for (var i = 0; i < numberedSeq.length; i++) {
+        if (! isIndexTaken(numberedSeq[i].index)) {
+            takenIndex.push(numberedSeq[i].index);
+            var stop = yield { 
+                node: wholeHTML[numberedSeq[i].index], 
+                distance: numberedSeq[i].value 
+            };
+
+            if (stop) {
+                break;
+            }
+        }
+    }
+}
 
 module.exports.getAllAncestorsOfNode = ancestors_all;
 module.exports.getNAncestorsOfNode = ancestors_n;
